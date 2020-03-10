da034273e7e80bb759b8cb33b3aa5eeb4f18467f
Howard Kennedy <hkennedyvi@gmail.com>
2020-03-10 10:54:57 -0700

    index and app files setup, employee component displays

---
+++ employeedir/src/components/employee.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Employee extends Component {
+  render() {
+    return (
+      <div>
+        <h1>I am an employee</h1>
+        <button>Hire Me</button>
+      </div>
+    );
+  }
+}
+
+export default Employee;
