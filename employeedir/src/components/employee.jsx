cfbac44bc49e9c21f663a4165a861899d1576910
Howard Kennedy <hkennedyvi@gmail.com>
2020-03-15 23:21:53 -0700

    employee map now includes index

--- employeedir/src/components/employee.jsx
+++ employeedir/src/components/employee.jsx
@@ -1,26 +1,17 @@
 import React, { Component } from "react";
 
 class Employee extends Component {
-  //We do not modify the state directly
+
   state = {
-    date: this.props.employee.date,
+    points: this.props.employee.points,
     name: this.props.employee.name,
     email: this.props.employee.email,
     phone: this.props.employee.phone,
     imageUrl: "https://picsum.photos/100",
-    tags: ["tag1", "tag2", "tag3"]
   };
 
-  //Event Handler
-  //   handleHire = () => {
-  //     console.log("Hire Me clicked");
-  //     //This method tells React the state of this component is going to change
-  //     //React will schedule a call to the render method
-  //     this.setState({ date: this.state.date + 1 });
-  //   };
-
   styles = {
-    fontSize: 100,
+    fontSize: 25,
     fontWeight: "bold"
   };
 
@@ -35,20 +26,19 @@ class Employee extends Component {
             {this.state.phone}
             <img src={this.state.imageUrl} alt="" />
             <span style={this.styles} className={this.getBadgeClasses()}>
-              {this.formatDate()}
+              {this.formatPoints()}
             </span>
             <button
-              onClick={() => this.props.onHire(this.props.employee)}
+              onClick={() => this.props.onIncrement(this.props.employee)}
               className="btn btn-primary btn-lg"
             >
-              Hire Me
+              + Brownie Points
         </button>
-        {/* Employee raises event, Employees handles event */}
         <button
           onClick={() => this.props.onDelete(this.props.employee.id)}
           className="btn btn-danger btn-sm m-2"
         >
-          Fire Me
+          Remove
         </button>
           </a>
         </ul>
@@ -58,13 +48,13 @@ class Employee extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.props.employee.date === 10 ? "warning" : "primary";
+    classes += this.props.employee.points === 100 ? "warning" : "primary";
     return classes;
   }
 
-  formatDate() {
-    const { date } = this.props.employee;
-    return date === 10 ? <h1>The tenth</h1> : date;
+  formatPoints() {
+    const { points } = this.props.employee;
+    return points === 100 ? <h1>One hundred</h1> : points;
   }
 }
 
--- employeedir/src/components/employees.jsx
+++ employeedir/src/components/employees.jsx
@@ -3,19 +3,19 @@ import Employee from "./employee";
 import API from "../utils/API";
 
 class Employees extends Component {
- 
+
   state = {
     employees: [
-      { id: 1, date: 10, name: "Joe Somebody", email: "gmail", phone: "503-123-4567", imageUrl: "https://picsum.photos/100" },
-      { id: 2, date: 15, name: "Joe Somebody", email: "gmail", phone: "503-123-4567", imageUrl: "https://picsum.photos/100" },
-      { id: 3, date: 23, name: "Joe Somebody", email: "gmail", phone: "503-123-4567", imageUrl: "https://picsum.photos/100" },
-      { id: 4, date: 30, name: "Joe Somebody", email: "gmail", phone: "503-123-4567", imageUrl: "https://picsum.photos/100" },
-      { id: 5, date: 10, name: "Joe Somebody", email: "gmail", phone: "503-123-4567", imageUrl: "https://picsum.photos/100" },
-      { id: 6, date: 15, name: "Joe Somebody", email: "gmail", phone: "503-123-4567", imageUrl: "https://picsum.photos/100" },
-      { id: 7, date: 23, name: "Joe Somebody", email: "gmail", phone: "503-123-4567", imageUrl: "https://picsum.photos/100" },
-      { id: 8, date: 30, name: "Joe Somebody", email: "gmail", phone: "503-123-4567", imageUrl: "https://picsum.photos/100" },
-      { id: 9, date: 30, name: "Joe Somebody", email: "gmail", phone: "503-123-4567", imageUrl: "https://picsum.photos/100" },
-      { id: 10, date: 30, name: "Joe Somebody", email: "gmail", phone: "503-123-4567", imageUrl: "https://picsum.photos/100" }
+      { id: 1, points: 100, name: "Joe Somebody", email: "gmail", phone: "503-123-4567", imageUrl: "https://picsum.photos/100" },
+      { id: 2, points: 150, name: "Joe Somebody", email: "gmail", phone: "503-123-4567", imageUrl: "https://picsum.photos/100" },
+      { id: 3, points: 230, name: "Joe Somebody", email: "gmail", phone: "503-123-4567", imageUrl: "https://picsum.photos/100" },
+      { id: 4, points: 300, name: "Joe Somebody", email: "gmail", phone: "503-123-4567", imageUrl: "https://picsum.photos/100" },
+      { id: 5, points: 100, name: "Joe Somebody", email: "gmail", phone: "503-123-4567", imageUrl: "https://picsum.photos/100" },
+      { id: 6, points: 150, name: "Joe Somebody", email: "gmail", phone: "503-123-4567", imageUrl: "https://picsum.photos/100" },
+      { id: 7, points: 230, name: "Joe Somebody", email: "gmail", phone: "503-123-4567", imageUrl: "https://picsum.photos/100" },
+      { id: 8, points: 300, name: "Joe Somebody", email: "gmail", phone: "503-123-4567", imageUrl: "https://picsum.photos/100" },
+      { id: 9, points: 300, name: "Joe Somebody", email: "gmail", phone: "503-123-4567", imageUrl: "https://picsum.photos/100" },
+      { id: 10, points: 300, name: "Joe Somebody", email: "gmail", phone: "503-123-4567", imageUrl: "https://picsum.photos/100" }
     ]
   };
 
@@ -24,13 +24,11 @@ class Employees extends Component {
   }
 
   findEmployees = () => {
-    const employees = [...this.state.employees];
-    let index = -1;
-    
+
     API.getEmployees()
       .then(res =>
-        res.data.results.map(employee => {
-          index++;
+        res.data.results.map((employee, index) => {
+          let employees = [...this.state.employees];
           let email = employee.email;
           let phone = employee.phone;
           let name = employee.name.first + " " + employee.name.last;
@@ -40,24 +38,26 @@ class Employees extends Component {
           employees[index].name = name;
           employees[index].imageUrl = image;
           this.setState({ employees });
+          console.log(employees[index]);
           return email;
         })
       )
+
       .catch(err => console.log(err));
   };
 
   handleReset = () => {
     const employees = this.state.employees.map(c => {
-      c.date = 0;
+      c.points = 0;
       return c;
     });
     this.setState({ employees });
   };
-  handleHire = employee => {
+  handleIncrement = employee => {
     const employees = [...this.state.employees];
     const index = employees.indexOf(employee);
     employees[index] = { ...employee };
-    employees[index].date++;
+    employees[index].points++;
     this.setState({ employees });
   };
   //Event handler
@@ -75,12 +75,12 @@ class Employees extends Component {
         >
           Reset Employees
         </button>
-        
+
         {this.state.employees.map(employee => (
           <Employee
             key={employee.id}
             onDelete={this.handleDelete}
-            onHire={this.handleHire}
+            onIncrement={this.handleIncrement}
             //employee object carries all the data about the employee
             // date={employee.date}
             // id={employee.id}
