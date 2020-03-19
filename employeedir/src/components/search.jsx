c3ccb1f70375a4b2d34847ca2a66e1afa5c2a380
Howard Kennedy <hkennedyvi@gmail.com>
2020-03-18 23:24:55 -0700

    search edit

--- employeedir/src/components/search.jsx
+++ employeedir/src/components/search.jsx
@@ -1,47 +1,6 @@
 // import React, { Component } from 'react';
 import React, { useState } from 'react';
 
-// class Search extends Component {
-
-//     state = {
-//         searchTerm: "search test"
-//     }
-
-//     handleSearch = event => {
-//         event.preventDefault();
-//         console.log("You searched for: " + this.state.searchTerm);
-//     }
-//     handleInputChange = event => {
-//         let userSearch = event.target.value;
-//         if (userSearch === "hello") {
-//             alert("Success!");
-//         } else
-//             console.log(userSearch);
-//     };
-
-//     render() {
-//         return (
-//             <div className="mt-3 mr-5 float-right">
-//                 <form className="search">
-//                     <div className="form-group">
-//                         <input
-//                             value={this.state.searchTerm}
-//                             // onChange={handleInputChange}
-//                             onChange={this.props.handleSearching}
-//                             placeholder="Find Employee..."></input>
-//                         <button type="submit"
-//                             className="btn btn-success"
-//                             onClick={this.handleSearch}
-//                         ><i className="fas fa-search"></i></button>
-//                     </div>
-//                 </form>
-//             </div>
-//         )
-//     }
-// }
-
-// export default Search;
-
 function Search(props) {
 
     const [searchTerm, setSearchTerm] = useState("");
@@ -87,3 +46,44 @@ function Search(props) {
 }
 
 export default Search;
+
+// class Search extends Component {
+
+//     state = {
+//         searchTerm: "search test"
+//     }
+
+//     handleSearch = event => {
+//         event.preventDefault();
+//         console.log("You searched for: " + this.state.searchTerm);
+//     }
+//     handleInputChange = event => {
+//         let userSearch = event.target.value;
+//         if (userSearch === "hello") {
+//             alert("Success!");
+//         } else
+//             console.log(userSearch);
+//     };
+
+//     render() {
+//         return (
+//             <div className="mt-3 mr-5 float-right">
+//                 <form className="search">
+//                     <div className="form-group">
+//                         <input
+//                             value={this.state.searchTerm}
+//                             // onChange={handleInputChange}
+//                             onChange={this.props.handleSearching}
+//                             placeholder="Find Employee..."></input>
+//                         <button type="submit"
+//                             className="btn btn-success"
+//                             onClick={this.handleSearch}
+//                         ><i className="fas fa-search"></i></button>
+//                     </div>
+//                 </form>
+//             </div>
+//         )
+//     }
+// }
+
+// export default Search;
