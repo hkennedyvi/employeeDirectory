634d1945f973e8c1170ead0b1d03b5d38e870ece
Howard Kennedy <hkennedyvi@gmail.com>
2020-10-16 12:24:38 -0700

    old files save

---
+++ employeedir/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+// import NavBar from "./components/navbar";
+import Employees from "./components/employees";
+// import Search from "./components/search"
+
+class App extends Component {
+
+
+  render() {
+    return (
+      <React.Fragment>
+        {/* <NavBar /> */}
+        {/* <Search /> */}
+        <Employees />
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
